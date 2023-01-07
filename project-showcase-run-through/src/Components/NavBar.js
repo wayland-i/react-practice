45a546c668397c6fbcec3fe48841a43bfe862e0d
Ian Iversen <ianiversen1@gmail.com>
2023-01-06 20:27:06 -0500

    feat: NavBar.js (and Header which lives there) mvp reached

--- project-showcase-run-through/src/Components/NavBar.js
+++ project-showcase-run-through/src/Components/NavBar.js
@@ -1,25 +1,47 @@
 import React from 'react';
 import { NavLink, Link } from "react-router-dom";
+import styled from "styled-components";
 
+const Navigation = styled.header`
+    /* display: flex; */
+    & > * {
+    margin: 15px;
+    }
+
+`
+
+const Header = styled.header`
+    display: flex;
+    justify-content: space-between;
+    
+    span {
+    margin: 15px;
+    }
+`
 
 
 function NavBar() {
-  return (
-    <div>
-        <Link to="/"> 
-            <span>//</span>
-        </Link>
-        <span>Project Showcase</span>
-        <NavLink to="/">
-            All Projects
-        </NavLink>
-        <NavLink to="/projects/new">
-            Add Project
-        </NavLink>
-        <NavLink to="/about">
-            About
-        </NavLink>
-    </div>
+    return (
+    <Header>
+        <div>
+            <Link to="/"> 
+                <span>//</span>
+            </Link>
+            <span>Project Showcase</span>
+        </div>
+
+        <Navigation>
+            <NavLink to="/">
+                All Projects
+            </NavLink>
+            <NavLink to="/projects/new">
+                Add Project
+            </NavLink>
+            <NavLink to="/about">
+                About
+            </NavLink>
+        </Navigation>
+    </Header>
   );
 }
 
