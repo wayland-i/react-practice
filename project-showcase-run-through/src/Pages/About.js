2e16a843fcf7146936ceeec045a986345093d332
Ian Iversen <ianiversen1@gmail.com>
2023-01-09 16:11:28 -0500

    feat: About page finished

--- project-showcase-run-through/src/Pages/About.js
+++ project-showcase-run-through/src/Pages/About.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 function About() {
   return (
-    <div>About</div>
+    <div style={{ "textAlign": "center", "marginTop": "10%"}}>
+      <h1>About Project Showcase</h1>
+      <br></br>
+      <p>This is a website that I made to introduce others to my portfolio. <br/> As a web developer, I have
+        built projects of all shapes and sizes. I hope that you find my work interesting!
+      </p>
+
+    </div>
   );
 }
 
